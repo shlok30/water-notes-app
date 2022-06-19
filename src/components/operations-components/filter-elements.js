d024d05fb6621c55bc47aef3268975eae993dbe7
Shlok Mehra <shlokmehra@rocketmail.com>
2022-06-19 14:28:39 +0530

    Feat : Filter Toggles

--- src/components/operations-components/filter-elements.js
+++ src/components/operations-components/filter-elements.js
@@ -2,11 +2,13 @@ import { useOperations } from "../../context/operations-context"
 
 const FilterElements = ({label}) => {
 
-    const {operationsState : {labelFilters}} = useOperations()
+    const {operationsState : {labelFilters},operationsDispatch} = useOperations()
+
+    console.log("operations state",labelFilters)
 
     return(
         <div className="flex gap-m align-center">
-            <input type = "checkbox" checked = {labelFilters[label] === true}/>
+            <input type = "checkbox" checked = {labelFilters[label] === true} onChange = {() => operationsDispatch({type : "LABELS", payload : label})}/>
             <p>{label}</p>
         </div>
     )
--- src/reducers/operationsReducer.js
+++ src/reducers/operationsReducer.js
@@ -3,7 +3,7 @@ const operationsReducer = (state,{type,payload}) => {
     switch(type){
         case "LABELS":
             if(payload in state.labelFilters){
-                return {...state,labelFilters : {...state.labelFilters, [payload] : !state[payload]}}
+                return {...state,labelFilters : {...state.labelFilters, [payload] : !state.labelFilters[payload]}}
             }
             return {...state,labelFilters : {...state.labelFilters,[payload] : true}}
         default:
