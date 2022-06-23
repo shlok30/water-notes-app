608a1e476aeb15f204c8f3b0093ee0bcfbfc169c
Shlok Mehra <shlokmehra@rocketmail.com>
2022-06-23 12:44:17 +0530

    Feat : Editing

--- src/context/notes-functions/editNote.js
+++ src/context/notes-functions/editNote.js
@@ -4,7 +4,11 @@ const editNote = (note,id,userDispatch,notesDispatch) => {
     const token = localStorage.getItem("userToken")
     axios
      .post(`/api/notes/${id}`,{note},{headers : {authorization:token}})
-     .then(res => console.log("Note was updated",res.data))
+     .then(res => {
+        console.log("Note was updated",res.data)
+        notesDispatch({type : "RESET"})
+        userDispatch({type : "ADD_NOTE", payload : res.data.notes})
+     })
      .catch(err => console.log(err))
 }
 
