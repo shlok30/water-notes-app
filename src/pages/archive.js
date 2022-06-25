71e69d84f93462dddba11e195e5fb8469be33011
Shlok Mehra <shlokmehra@rocketmail.com>
2022-06-25 16:22:10 +0530

    Added no notes text

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -8,6 +8,7 @@ const ArchivePage = () => {
     return(
         <div className="padding-l-inline grow-1">
             <h2 className="h-l center-text">Archived Notes</h2>
+            {archives.length === 0 ? <p className="wt-400 text-m primary-text-colour center-text m5-top">No Notes in Archive</p> : null }
             <div className="flex align-start gap-xl m3-top padding-l-inline">
                 {renderNoteCards(archives)}
             </div>
--- src/pages/trash.js
+++ src/pages/trash.js
@@ -8,6 +8,7 @@ const TrashPage = () => {
     return(
         <div className="padding-l-inline grow-1">
             <h2 className="h-l center-text">Trash</h2>
+            {trash.length === 0 ? <p className="wt-400 text-m primary-text-colour center-text m5-top">No Notes in Trash</p> : null }
             <div className="flex align-start gap-xl m3-top padding-l-inline">
                 {renderNoteCards(trash)}
             </div>
