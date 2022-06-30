7b86d36dc86f720f7098495ce2a20b83d8a71de3
Shlok Mehra <shlokmehra@rocketmail.com>
2022-07-01 00:13:45 +0530

    Feat : Added error message if wrong credentials are entered

--- src/auth-functions/handleLogin.js
+++ src/auth-functions/handleLogin.js
@@ -1,14 +1,17 @@
 import axios from "axios"
 
-const handleLogin = (email,password,userDispatch,navigate) => {
+const handleLogin = (email,password,userDispatch,navigate,setError,error) => {
     axios
      .post("/api/auth/login",{email,password})
      .then(res => {
         localStorage.setItem("userToken",res.data.encodedToken)
         userDispatch({type : "LOGIN", payload : res.data.foundUser})
+        if(error){
+         setError("")
+        }
         navigate("/homepage")
      })
-     .catch(err => console.log(err))
+     .catch(err => setError(err.response.data.errors))
 }
 
 export default handleLogin
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 import { useNavigate, Link } from "react-router-dom"
 import handleLogin from "../auth-functions/handleLogin"
 import { useUser } from "../context/user-context"
@@ -8,6 +9,12 @@ const LoginPage = () => {
 
     const navigate = useNavigate()
 
+    const [email,setEmail] = useState("")
+
+    const [password,setPassword] = useState("")
+
+    const [error,setError] = useState("")
+
     return(
     
         <div className = "container auth-container center-block m6-top light-border w-35">
@@ -15,12 +22,12 @@ const LoginPage = () => {
             <div className = "primary-text center-text text-m bold">Login</div>
             <div className = "padding-s">
                 <label htmlFor="demo" className="input-label">Email Address</label>
-                <input type = "email" id = "demo" className = "input-field m2-top full-width" style = {{display:'block',width:'100%'}} placeholder="Enter Email" />
+                <input type = "email" id = "demo" value = {email} onChange = {(e) => setEmail(e.target.value)} className = "input-field m2-top full-width" style = {{display:'block',width:'100%'}} placeholder="Enter Email" />
             </div>
             
             <div className="padding-s flex flex-column">
                 <label htmlFor="demo1" className="input-label">Enter Password</label>
-                <input type = "password" id = "demo1" className = "input-field m2-top full-width" style = {{display:'block',width:'100%'}}  placeholder="Enter Password" />
+                <input type = "password" id = "demo1" value = {password} onChange = {(e) => setPassword(e.target.value)} className = "input-field m2-top full-width" style = {{display:'block',width:'100%'}}  placeholder="Enter Password" />
             </div>
             
     
@@ -32,11 +39,12 @@ const LoginPage = () => {
                 <a href="" className = "primary-text">Forgot Password</a>
             </div>
 
-            <button className="btn btn-secondary full-width m2-top">Login</button>
+            <button className="btn btn-secondary full-width m2-top" onClick = {() => handleLogin(email,password,userDispatch,navigate,setError,error)}>Login</button>
             <button className="btn btn-success full-width m2-top" onClick={() => handleLogin("guest@gmail.com","guest123",userDispatch,navigate)}>Guest Login</button>
             <div className = "m2-top">
                 <Link to = "/signup" className = "primary-text">Create New Account?</Link>
             </div>
+            <p className = "error-text-colour m2-top">{error}</p>
         </div>
     )
 }
